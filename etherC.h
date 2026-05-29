034e837343b9fd01f478cd0328c0d614cc0e1db5
EtherOS <tylerthatgod@gmail.com>
2026-05-28 23:19:52 -0500

    Add files via upload

---
+++ etherC.h
@@ -0,0 +1,17 @@
+
+#define bes buffer
+#define eh int
+#define ex extern
+#define ei char
+#define ie void
+#define eq else
+#define ej index
+#define er while
+#define printf ether
+#define  star return
+#define flower if
+#define print_string ether_string
+
+
+#define make_command(name, code) \
+if (str_eq(cmd + i, name )) { code; return; }
